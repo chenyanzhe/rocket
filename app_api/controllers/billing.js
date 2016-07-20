ecd0d10ec541db0bcab5ac8bdc668b55eec697b3
Yanzhe Chen <t-yachen@microsoft.com>
2016-07-20 15:30:06 +0800

    Add basic functions

--- app_api/controllers/billing.js
+++ app_api/controllers/billing.js
@@ -6,152 +6,138 @@ var sendJSONresponse = function(res, status, content) {
 module.exports.lastWeek = function(request, response) {
 
 	var async = require('async');
-/*
+
 	var accessToken;
 	var rateCard;
+	var cToken = '';
+	var aggregatedUsage = [];
+	var usageSegment;
 
-	function getToken(callback) {
-		var qs = require("querystring");
-		var http = require("https");
+	function getAccessToken(callback) {
+		var request = require("request");
 
-		var options = {
-		  "method": "POST",
-		  "hostname": "login.microsoftonline.com",
-		  "port": null,
-		  "path": "/microsoft.onmicrosoft.com/oauth2/token?api-version=1.0",
-		  "headers": {
-		    "content-type": "application/x-www-form-urlencoded",
-		    "cache-control": "no-cache",
-		    "postman-token": "460ef508-d723-8cde-95a1-06b1518f4339"
-		  }
-		};
+		var options = { method: 'POST',
+		  url: 'https://login.microsoftonline.com/microsoft.onmicrosoft.com/oauth2/token',
+		  qs: { 'api-version': '1.0' },
+		  headers: 
+		   { 'postman-token': 'ef193fec-bdf3-8d7a-769a-0de8dbe529d5',
+		     'cache-control': 'no-cache',
+		     'content-type': 'application/x-www-form-urlencoded' },
+		  form: 
+		   { grant_type: 'client_credentials',
+		     resource: 'https://management.core.windows.net/',
+		     client_id: '8a82026c-0ed3-4023-953e-50a95005c3e0',
+		     client_secret: 'tyachen123' } };
 
-		var req = http.request(options, function (res) {
-		  var chunks = [];
-
-		  res.on("data", function (chunk) {
-		    chunks.push(chunk);
-		  });
-
-		  res.on("end", function () {
-		    var body = Buffer.concat(chunks);
-		    accessToken = JSON.parse(body.toString());
-		    console.log("get access token");
-		    callback(null);
-		  });
+		request(options, function (err, res, body) {
+		  if (err) throw new Error(err);
+		  accessToken = JSON.parse(body);
+		  console.log("Access token done!");
+		  callback(null);
 		});
-
-		req.write(qs.stringify({ grant_type: 'client_credentials',
-		  resource: 'https://management.core.windows.net/',
-		  client_id: '8a82026c-0ed3-4023-953e-50a95005c3e0',
-		  client_secret: 'tyachen123' }));
-		req.end();
 	}
 
-	function getRateCard(callback) {
-		var http = require("https");
+	function getRateCard(innerCallback) {
+		var request = require("request");
 
-		var options = {
-		  "method": "GET",
-		  "hostname": "management.azure.com",
-		  "port": null,
-		  "path": "/subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/providers/Microsoft.Commerce/RateCard?api-version=2015-06-01-preview&$filter=OfferDurableId%20eq%20'MS-AZR-0015P'%20and%20Currency%20eq%20'USD'%20and%20Locale%20eq%20'en-US'%20and%20RegionInfo%20eq%20'US'",
-		  "headers": {
-		    "content-type": "application/json",
-		    "authorization": accessToken.token_type + " " + accessToken.access_token,
-		    "cache-control": "no-cache",
-		    "postman-token": "ba488ac6-c8d6-c02a-6e23-38f44ceb2ee6"
-		  }
-		};
+		var options = { method: 'GET',
+		  url: 'https://management.azure.com/subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/providers/Microsoft.Commerce/RateCard',
+		  qs: 
+		   { 'api-version': '2015-06-01-preview',
+		     '$filter': 'OfferDurableId eq \'MS-AZR-0015P\' and Currency eq \'USD\' and Locale eq \'en-US\' and RegionInfo eq \'US\'' },
+		  headers: 
+		   { 'postman-token': 'f7687100-0be5-42dd-2d28-96f88fa7b892',
+		     'cache-control': 'no-cache',
+		     "authorization": accessToken.token_type + " " + accessToken.access_token,
+		     'content-type': 'application/json' } };
 
-		var req = http.request(options, function (res) {
-		  var chunks = [];
-
-		  res.on("data", function (chunk) {
-		    chunks.push(chunk);
-		  });
-
-		  res.on("end", function () {
-		    var body = Buffer.concat(chunks);
-		    rateCard = JSON.parse(body.toString());
-		    console.log("get rate card");
-		    callback(null);
-		  });
+		request(options, function (error, res, body) {
+		  if (error) throw new Error(error);
+		  rateCard = JSON.parse(body);
+		  console.log("RateCard done!");
+		  innerCallback(null);
 		});
+	}
 
-		req.end();
+	function hasNextPage() {
+		return cToken != '';
+	}
+
+	function getAggregatedUsageHelper(innerCallback2) {
+		console.log("Trying get page with cToken: " + cToken);
+		var request = require("request");
+
+		var options = { method: 'GET',
+		  url: 'https://management.azure.com/subscriptions/c4528d9e-c99a-48bb-b12d-fde2176a43b8/providers/Microsoft.Commerce/UsageAggregates',
+		  qs: 
+		   { 'api-version': '2015-06-01-preview',
+		     reportedStartTime: '2016-07-18T00:00:00+00:00',
+		     reportedEndTime: '2016-07-20T00:00:00+00:00',
+		     aggregationGranularity: 'Daily',
+		     showDetails: 'true' },
+		  headers: 
+		   { 'postman-token': '5dedff2e-5405-205a-593f-aabf164b6888',
+		     'cache-control': 'no-cache',
+		     'content-type': 'application/json',
+		     'authorization': accessToken.token_type + " " + accessToken.access_token } };
+
+		if (cToken != '') options.qs.continuationToken = cToken;
+
+		request(options, function (error, res, body) {
+		  if (error) throw new Error(error);
+		  usageSegment = JSON.parse(body);
+		  aggregatedUsage.push(usageSegment);
+		  if (usageSegment.nextLink) {
+		  	var idx = usageSegment.nextLink.lastIndexOf("=");
+		  	cToken = usageSegment.nextLink.substr(idx + 1);
+		  	cToken = decodeURIComponent(cToken);
+		  } else {
+		  	cToken = '';
+		  }
+		  console.log("One page done!");
+		  innerCallback2(null);
+		});
+	}
+
+	function getAggregatedUsage(innerCallback) {
+		async.doWhilst(
+		    getAggregatedUsageHelper,
+		    hasNextPage,
+		    function (err) {
+		    	console.log("final callback")
+				if (err) {
+					sendJSONresponse(response, 404, err);
+					return;
+				}
+				innerCallback(null);
+			}
+		);
+	}
+
+	function getData(callback) {
+		async.parallel([
+		    getRateCard,
+		    getAggregatedUsage
+		], function (err) {
+			if (err) {
+				sendJSONresponse(response, 404, err);
+				return;
+			}
+			callback(null);
+		});
 	}
 
 	async.series([
-	    getToken,
-	    getRateCard
+	    getAccessToken,
+	    getData
 	], function (err) {
 		if (err) {
 			sendJSONresponse(response, 404, err);
 			return;
 		}
-	    sendJSONresponse(response, 200, rateCard);
+		var nPages = aggregatedUsage.length;
+		console.log("aggregatedUsage have " + nPages + " pages");
+		sendJSONresponse(response, 200, aggregatedUsage[nPages-1]);
 	});
-
-*/
-/*
-
-	var gvar1;
-	var gvar2;
-
-	function takes5Seconds(callback) {
-	    console.log('Starting 5 second task');
-	    setTimeout( function() { 
-	        console.log('Just finshed 5 seconds');
-	        gvar1 = "var1";
-	        callback(null, 'five');
-	    }, 5000);
-	}   
-
-	function takes2Seconds(callback) {
-	    console.log('Starting 2 second task');
-	    setTimeout( function() { 
-	        console.log('Just finshed 2 seconds');
-	        gvar2 = "var2";
-	        callback(null, 'two');
-	    }, 2000); 
-	}
-
-	async.series([
-	    takes2Seconds,
-	    takes5Seconds
-	], function (err, results) {
-		console.log(gvar1);
-		console.log(gvar2);
-		var json_data = {"name": results[0], "pass": results[1]};
-	    // Here, results is an array of the value from each function
-	    sendJSONresponse(response, 200, json_data)
-	    // console.log(results); // outputs: ['two', 'five']
-	});
-*/
-
-
-
-	var request = require("request");
-
-	var options = { method: 'POST',
-	  url: 'https://login.microsoftonline.com/microsoft.onmicrosoft.com/oauth2/token',
-	  qs: { 'api-version': '1.0' },
-	  headers: 
-	   { 'postman-token': 'ef193fec-bdf3-8d7a-769a-0de8dbe529d5',
-	     'cache-control': 'no-cache',
-	     'content-type': 'application/x-www-form-urlencoded' },
-	  form: 
-	   { grant_type: 'client_credentials',
-	     resource: 'https://management.core.windows.net/',
-	     client_id: '8a82026c-0ed3-4023-953e-50a95005c3e0',
-	     client_secret: 'tyachen123' } };
-
-	request(options, function (err, res, body) {
-	  if (err) throw new Error(err);
-
-	  console.log(body);
-	  sendJSONresponse(response, 200, JSON.parse(body));
-	});
-
+	
 };
