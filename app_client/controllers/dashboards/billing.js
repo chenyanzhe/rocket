72ebd778993bdbbd04aac27d44d9c8edb6ac3837
Yanzhe Chen <t-yachen@microsoft.com>
2016-07-27 16:20:13 +0800

    Add who to blame block

--- app_client/controllers/dashboards/billing.js
+++ app_client/controllers/dashboards/billing.js
@@ -1,3 +1,72 @@
+var nameMapping = [
+    { prefix: 'abel', alias: 'abel' },
+    { prefix: 'andy', alias: 'xiazhang' },
+    { prefix: 'liz', alias: 'lizzha' },
+    { prefix: 'likezh', alias: 'likezh' },
+    { prefix: 'qingfu', alias: 'qingfu' },
+    { prefix: 'minli', alias: 'minli' },
+    { prefix: 'harpal', alias: 'v-harpal' },
+    { prefix: 'lili', alias: 'v-lide' },
+    { prefix: 'kx', alias: 'v-kaxiao' },
+    { prefix: 'yz', alias: 'yuezha' },
+    { prefix: 'binxi', alias: 'binxi' },
+    { prefix: 'andliu', alias: 'andliu' },
+    { prefix: 'decui', alias: 'decui' },
+    { prefix: 'thomas', alias: 'huishao' },
+    { prefix: 'ts', alias: 'huishao' },
+    { prefix: 'scott', alias: 'mingzhan' },
+    { prefix: 'wei', alias: 'v-lii' },
+    { prefix: 'xhx', alias: 'v-hoxian' },
+    { prefix: 'ica', alias: 'CLOUD' },
+    { prefix: 'azperf', alias: 'PERF' },
+    { prefix: 'testimage', alias: 'TestImage' },
+    { prefix: 'av', alias: 'v-avchat' },
+    { prefix: 'ram', alias: 'v-ranmut' },
+    { prefix: 'siva', alias: 'v-sirebb' },
+    { prefix: 'srm', alias: 'v-srm' },
+    { prefix: 'ss', alias: 'v-shisav' },
+    { prefix: 'andy', alias: 'v-feile' },
+    { prefix: 'zhongyi', alias: 'v-zhongz' },
+    { prefix: 'jm', alias: 'jinmiao' },
+    { prefix: 'guwe', alias: 'guwe' },
+    { prefix: 'doliu', alias: 'doliu' },
+    { prefix: 'joey', alias: 'v-shuihe' },
+    { prefix: 'honzhan', alias: 'honzhan' },
+    { prefix: 'shostc', alias: 'shostc' }
+];
+
+// name, cost
+function whoToBlame(billingInfo) {
+    var blameInfo = [ {name: 'unknown', cost: 0} ];
+    var ret = [];
+    for (var i = 0; i < billingInfo.length; i++) {
+        var found = false;
+        if (typeof billingInfo[i].resourceName === "string") {
+            for (var j = 0; j < nameMapping.length; j++) {
+                if (billingInfo[i].resourceName.toLowerCase().startsWith(nameMapping[j].prefix) ||
+                    billingInfo[i].resourceGroup.toLowerCase().includes(nameMapping[j].prefix)) {
+                    found = true;
+                    if (typeof blameInfo[j + 1] === "undefined") {
+                        blameInfo[j + 1] = {name: nameMapping[j].alias, cost: billingInfo[i].totalCost};
+                    } else {
+                        blameInfo[j + 1].cost += billingInfo[i].totalCost;
+                    }
+                    break;
+                }
+            }
+        }
+        if (!found) {
+            blameInfo[0].cost += billingInfo[i].totalCost;
+        }
+    }
+    for (var i = 0; i < blameInfo.length; i++) {
+        if (typeof blameInfo[i] !== "undefined")
+            ret.push(blameInfo[i]);
+    }
+    ret.sort( function(a,b) {return b.cost - a.cost} );
+    return ret;
+}
+
 function billingCtrl($scope, $timeout, billingService) {
     var defaultEnd = moment().startOf('day');
     var defaultStart = moment(defaultEnd).subtract(1, 'week');
@@ -8,6 +77,7 @@ function billingCtrl($scope, $timeout, billingService) {
     billingService.getBillingFunc(startArg, endArg)
         .success(function (data) {
             $scope.billingInfo = data;
+            $scope.blameInfo = whoToBlame(data);
             $timeout(function () {
                 $('.footable').trigger('footable_redraw');
             }, 100);
@@ -23,6 +93,7 @@ function billingCtrl($scope, $timeout, billingService) {
             .success(function (data) {
                 $('.footable').trigger('footable_initialize');
                 $scope.billingInfo = data;
+                $scope.blameInfo = whoToBlame(data);
                 $timeout(function () {
                     $('.footable').trigger('footable_redraw');
                 }, 100);
