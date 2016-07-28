2b12a48c773035ec51a38770e0721b0a8d098836
Yanzhe Chen <t-yachen@microsoft.com>
2016-07-28 14:33:22 +0800

    Adjust project banner

--- app_client/controllers/main.js
+++ app_client/controllers/main.js
@@ -8,9 +8,7 @@
  */
 function MainCtrl() {
 
-    this.userName = 'Example user';
-    this.helloText = 'Welcome in SeedProject';
-    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
+    this.userName = 'Rocket';
 
      /**
      * states - Data used in Advanced Form view for Chosen plugin
